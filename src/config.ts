08d53c7a2d31620eaab0280c890a9b306bc3a861
晴琉琉 <107309389+qingliuliu@users.noreply.github.com>
2023-03-03 23:02:32 +0800

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,29 @@
+const config = {
+  github: {
+    username: 'qingliuliu',
+    repository: 'blog',
+    token: ['ghp_v51yBW7xRqjbTNJ', 'XqnOqVILxtuaoSq0gSkKy'],
+  },
+  leancloud: {
+    appId: '9qauRvYLKevbm9KumN8pC1y8-MdYXbMMI',
+    appKey: '3S7I5xwlfhqqwOd8AFTJv7ON',
+  },
+  contact: {
+    github: 'https://github.com/qingliuliu',
+    twitter: 'https://twitter.com/chnshy60136683',
+    telegram: 'https://twitter.com/chnshy60136683',
+    email: 'mailto://z2161917015@163.com',
+    music: 'https://music.163.com/#/user/home?id=4991127287',
+    blog: 'https://chanshiyu.icu',
+  },
+  gitalk: {
+    clientID: '10e421878630a2bebdd4',
+    clientSecret: '71a9e179d6dd7d6af9d6be260be11d3c67616c19',
+    repo: 'Comment',
+    owner: 'qingliuliu',
+    admin: ['qingliuliu'],
+    distractionFreeMode: false,
+  },
+}
+
+export default config
